8d956b203d5061dc19208210319d851b8753c767
Edward Weston <eddweston@me.com>
2016-04-16 21:58:09 +0100

    next

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const index = () => {}
+  const domHook = document.getElementById('app')
+  ReactDom.render(<HelloWorld />, domHook)
+}
+
+index()
