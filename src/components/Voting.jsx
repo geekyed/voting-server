f58a6ab4226fa9784fde8bad0733d10fe3a08f5a
Edward Weston <eddweston@me.com>
2016-04-18 21:14:49 +0100

    formating

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 const Voting = ({ pair }) => {
   return <div className='voting'>
       {
-        pair.map(
-          entry => <button key={entry}>
+        pair.map(entry =>
+          <button key={entry}>
           <h1>{entry}</h1>
-          </button>
-        )
+          </button>)
       }
     </div>
 }
